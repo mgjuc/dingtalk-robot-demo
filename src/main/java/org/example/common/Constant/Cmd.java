c65b47cee4c30a87ee7198cf1fd8cea79051693a
yanguoqiang <yanguoqiang@yiritech.com>
2024-05-14 15:29:20 +0800

    更新配置

---
+++ src/main/java/org/example/common/Constant/Cmd.java
@@ -0,0 +1,8 @@
+package org.example.common.Constant;
+
+public class Cmd {
+    public static final String ALL = "all" + " ";
+    public static final String DONE = "done" + " ";
+
+    public static final String ADD = "add" + " ";
+}
